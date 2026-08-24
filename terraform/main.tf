28a2b6b6e41602ad01bd0275679f0fd823d1f90b
danielpredel <danielpreciadodelgadillo@gmail.com>
2026-08-23 23:33:09 -0600

    feat(infra): add ec2 resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -144,3 +144,35 @@ resource "aws_s3_bucket" "app" {
     }
   )
 }
+
+
+# EC2 resources
+data "aws_ami" "al2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*"]
+  }
+}
+
+resource "aws_instance" "app" {
+  ami                    = data.aws_ami.al2023.id
+  instance_type          = "t3.micro"
+  key_name               = local.key_pair_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_app.id]
+  subnet_id              = aws_subnet.public.id
+  iam_instance_profile   = aws_iam_instance_profile.ec2_app.name
+
+  user_data = templatefile("${path.module}/user-data.sh", {
+    bucket_name = local.bucket_name
+  })
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = local.instance_name
+    }
+  )
+}
