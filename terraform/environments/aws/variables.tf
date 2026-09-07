57847e3efb3784d10e614b5c9c8480f9a1950020
danielpredel <danielpreciadodelgadillo@gmail.com>
2026-09-07 17:30:52 -0600

    feat(infra): add variables to ec2 module to define the AMI dynamically

--- terraform/environments/aws/variables.tf
+++ terraform/environments/aws/variables.tf
@@ -17,3 +17,18 @@ variable "region_abbreviation" {
   type    = string
   default = "use1"
 }
+
+variable "ami_owner" {
+  type        = string
+  default     = "amazon"
+}
+
+variable "ami_name_pattern" {
+  type        = string
+  default     = "al2023-ami-*"
+}
+
+variable "ami_architecture" {
+  type        = string
+  default     = "x86_64"
+}
