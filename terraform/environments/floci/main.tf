21d73e7569b4c9e6e4f11a836470fb96277c70a7
danielpredel <danielpreciadodelgadillo@gmail.com>
2026-09-07 17:34:26 -0600

    feat(infra): modify user data to match an ubuntu instance with a service to execute the app. Send Floci's inner IP addr to use in app's service after restarts

--- terraform/environments/floci/main.tf
+++ terraform/environments/floci/main.tf
@@ -34,6 +34,7 @@ module "ec2" {
   instance_profile_name = module.iam.instance_profile_name
 
   user_data = templatefile("${path.module}/user-data.sh", {
-    bucket_name = local.s3.bucket_name
+    bucket_name      = local.s3.bucket_name
+    aws_endpoint_url = "http://172.17.0.2:4566"
   })
 }
