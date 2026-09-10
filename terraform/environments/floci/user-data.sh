c90486c22fe2d9734bee906f3803d2c5c5ea3004
danielpredel <danielpreciadodelgadillo@gmail.com>
2026-09-10 15:53:54 -0600

    feat(infra): add CloudWatch Agent setup in Floci's user-data

--- terraform/environments/floci/user-data.sh
+++ terraform/environments/floci/user-data.sh
@@ -28,10 +28,7 @@ echo "${bucket_name}" > ./src/.bucket_name
 # Download app's dependencies
 /root/.local/bin/uv sync
 
-# Start app
-# /root/.local/bin/uv run uvicorn src.main:app --host 0.0.0.0 --port 8000
-
-# Create systemd service
+# Create systemd service for the app
 cat > /etc/systemd/system/aws-foundations.service <<EOF
 [Unit]
 Description=AWS Foundations FastAPI App
@@ -39,8 +36,8 @@ After=network.target
 
 [Service]
 Environment="AWS_ENDPOINT_URL=${aws_endpoint_url}"
-Environment="AWS_EC2_METADATA_SERVICE_ENDPOINT=http://172.17.0.2:9169"
-Environment="AWS_DEFAULT_REGION=us-east-1"
+Environment="AWS_EC2_METADATA_SERVICE_ENDPOINT=${aws_ec2_metadata_service_endpoint}"
+Environment="AWS_DEFAULT_REGION=${region}"
 User=root
 WorkingDirectory=/root/app
 ExecStart=/root/.local/bin/uv run uvicorn src.main:app --host 0.0.0.0 --port 8000
@@ -54,3 +51,49 @@ EOF
 # Enable and start service
 systemctl daemon-reload
 systemctl enable --now aws-foundations.service
+
+# Install CloudWatch Agent
+cd /tmp
+wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E amazon-cloudwatch-agent.deb
+
+# Create CloudWatch Configuration File
+cat > /opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-agent.json <<EOF
+{
+    "agent": {
+        "region": "${region}"
+    },
+    "logs": {
+        "endpoint_override": "${aws_endpoint_url}",
+        "logs_collected": {
+            "journald": {
+                "collect_list": [
+                    {
+                        "units": [
+                        "aws-foundations.service"
+                        ],
+                        "log_group_name": "/aws/ec2/aws-foundations",
+                        "log_stream_name": "{instance_id}"
+                    }
+                ]
+            }
+        }
+    }
+}
+EOF
+
+# Add Floci endpoints to CloudWatch Agent systemd service configuration 
+sudo mkdir -p /etc/systemd/system/amazon-cloudwatch-agent.service.d
+
+sudo tee /etc/systemd/system/amazon-cloudwatch-agent.service.d/floci.conf > /dev/null <<EOF
+[Service]
+Environment="AWS_ENDPOINT_URL=${aws_endpoint_url}"
+Environment="AWS_EC2_METADATA_SERVICE_ENDPOINT=${aws_ec2_metadata_service_endpoint}"
+EOF
+
+sudo systemctl daemon-reload
+
+# Start the agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+  -a fetch-config -m ec2 -s \
+  -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-agent.json
